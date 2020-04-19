fcfea37eab367a3dfdb160a1484bf6cecb7c890c
kamil.konopka <kamil.konopka@jeppesen.com>
2020-04-19 13:19:59 +0200

    added get user decorator to avoid code duplication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import {UserRepository} from "./user.repository";
 import {User} from "./user.entity";
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt-strategy';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { PassportModule } from '@nestjs/passport';
     }),
     TypeOrmModule.forFeature([User, UserRepository])],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
